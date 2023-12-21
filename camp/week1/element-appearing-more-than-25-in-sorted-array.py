eec75ba88aa12009cd91c053c0b41aab12183a4c
Lij Naol <135800478+Naol86@users.noreply.github.com>
2023-12-21 19:41:12 +0300

    Added solution for Element Appearing More Than 25% In Sorted Array

--- camp/week1/element-appearing-more-than-25-in-sorted-array.py
+++ camp/week1/element-appearing-more-than-25-in-sorted-array.py
@@ -1,14 +1,8 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        dic = {}
-        leng = len(arr)
+        n = len(arr) * 0.25
+        dic = defaultdict(int)
         for i in arr:
-            if i in dic:
-                dic[i] += 1
-                if dic[i] > leng *0.25:
-                    return i
-            else:
-                dic[i] = 1
-                if dic[i] > leng*0.25:
-                    return i
-        return -1
+            dic[i] += 1
+            if dic[i] > n:
+                return i
