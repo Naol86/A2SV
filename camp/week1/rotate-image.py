6967ccb3405e6f8f8e8d5a60517bbcfc6feafb9f
Lij Naol <135800478+Naol86@users.noreply.github.com>
2023-12-22 17:46:23 +0300

    Added solution for Rotate Image

---
+++ camp/week1/rotate-image.py
@@ -0,0 +1,14 @@
+import numpy as np
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        new = []
+        lis = np.array(matrix)
+        for i in range(len(matrix)):
+            temp = list(lis[0:,i])
+            new += [temp[::-1]]
+        for i in range(len(matrix)):
+            matrix[i] = new[i]
+        return matrix
