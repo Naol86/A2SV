07edcb18fd07f2691f83cbe97f676ed6ea8738db
Lij Naol <135800478+Naol86@users.noreply.github.com>
2023-12-24 10:50:17 +0300

    Added solution for Card Flipping Game

---
+++ camp/week1/card-flipping-game.py
@@ -0,0 +1,23 @@
+from collections import Counter
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        numbers = defaultdict(set)
+        dont_care = set([])
+        leng = len(fronts)
+        for i in range(leng):
+            numbers[fronts[i]].add(i)
+        for i in range(leng):
+            if backs[i] in dont_care:
+                continue
+            elif backs[i] in numbers:
+                if i in numbers[backs[i]]:
+                    dont_care.add(backs[i])
+                    numbers.pop(backs[i])
+            else:
+                numbers[backs[i]].add(i)
+        lis = list(numbers.keys())
+        if len(lis) == 0:
+            return 0
+        lis.sort()
+        # print(lis)
+        return lis[0]
