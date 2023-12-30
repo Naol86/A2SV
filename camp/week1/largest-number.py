79afa792f592284a3e1f2b38db0b95c6a360b6ad
Lij Naol <135800478+Naol86@users.noreply.github.com>
2023-12-30 08:31:59 +0300

    Added solution for Largest Number

---
+++ camp/week1/largest-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for index, value in enumerate(nums):
+            nums[index] = str(value)
+        
+        def compare(n1, n2):
+            if n1 + n2 > n2 + n1:
+                return -1
+            return 1
+        
+        nums = sorted(nums, key=cmp_to_key(compare))
+        return str(int("".join(nums)))
