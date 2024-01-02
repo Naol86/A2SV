bfb095c4f29e47300eb0ab4a2ad708280382fbef
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-01-02 11:27:59 +0300

    Added solution for Reduction Operations to Make the Array Elements Equal

---
+++ camp/week1/reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,20 @@
+import numpy as np
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        temp = np.array([])
+        _min = nums[0]
+        for i in nums:
+            if i < _min:
+                _min = i
+            if i not in temp:
+                # temp = temp
+                temp = np.append(temp, i)
+        temp.sort()
+        dic = {}
+        for i in range(len(temp)):
+            dic[temp[i]] = i
+        ans = 0
+        for i in nums:
+            ans += dic[i]
+
+        return ans
