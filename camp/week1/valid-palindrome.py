c46b7e65a627ea19fc04f42e890fa53e9af84bcf
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-01-02 17:30:18 +0300

    Added solution for Valid Palindrome

---
+++ camp/week1/valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a = 0
+        b = len(s) -1
+        while a < b:
+            if not s[a].isalpha() and not s[a].isdigit() or s[a] == " ":
+                a+=1
+                continue
+            if not s[b].isalpha() and not s[b].isdigit() or s[b] == " ":
+                b-=1
+                continue
+            elif s[a].lower() != s[b].lower():
+                return False
+            else:
+                a+=1
+                b-=1
+        return True
