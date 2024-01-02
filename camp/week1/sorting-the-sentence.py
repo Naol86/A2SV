71baaa61e391ad6eafa1318e63827092745f920d
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-01-02 11:26:00 +0300

    Added solution for Sorting the Sentence

--- camp/week1/sorting-the-sentence.py
+++ camp/week1/sorting-the-sentence.py
@@ -1,18 +1,7 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        lis=s.split()
-        di={}
+        lis = s.split()
+        ans = [0] * len(lis)
         for i in lis:
-            ind=[]
-            for j in range(len(i)-1,0,-1):
-                if not i[j].isdigit():
-                    break
-                ind.insert(0,i[j])
-            st=''.join(ind)
-            di[int(st)]=i[:0-len(st)]
-        Index=sorted(di)
-        ans=''
-        for i in Index:
-            ans=ans+di[i]+' '
-        ans=ans[:-1]
-        return ans
+            ans[int(i[-1]) - 1] = i[:-1]
+        return " ".join(ans)
