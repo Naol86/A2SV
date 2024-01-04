6411f3a683e36b758bd980c1a5a0b7b3e92cc0c1
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-01-04 19:52:42 +0300

    Added solution for Diagonal Traverse

--- camp/week1/diagonal-traverse.py
+++ camp/week1/diagonal-traverse.py
@@ -1,49 +1,14 @@
+from collections import defaultdict
 class Solution:
-    def __init__(self):
-        self.up = True
-    def go_up(self, row,col, mat):
-        if self.can_we(row - 1, col + 1, mat):
-            return [row - 1, col + 1]
-        self.up = False
-        if self.can_we(row, col + 1, mat):
-            return [row, col + 1]
-        if self.can_we(row + 1, col, mat):
-            return [row + 1, col]
-        return []
-    def go_down(self, row, col, mat):
-        if self.can_we(row + 1, col - 1, mat):
-            return [row + 1, col - 1]
-        self.up = True
-        if self.can_we(row + 1, col, mat):
-            return [row + 1, col]
-        if self.can_we(row, col + 1, mat):
-            return [row, col + 1]
-        return []
-    def can_we(self, row, col, mat):
-        if row < 0 or col < 0:
-            return False
-        if row >= len(mat) or col >= len(mat[0]):
-            return False
-        return True
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        if len(mat) == 1:
-            return mat[0]
-        ans_list = []
-        row = 0
-        col = 0
-        while True:
-            if self.can_we(row, col, mat):
-                ans_list.append(mat[row][col])
-            if self.up:
-                temp = self.go_up(row, col, mat)
-                if len(temp) == 0:
-                    return ans_list
-                row = temp[0]
-                col = temp[1]
+        lis = defaultdict(list)
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                lis[i+j].append(mat[i][j])
+        ans = []
+        for i in lis:
+            if i % 2 == 0:
+                ans += lis[i][::-1]
             else:
-                temp = self.go_down(row, col, mat)
-                if len(temp) == 0:
-                    return ans_list
-                row = temp[0]
-                col = temp[1]
-        return []
+                ans += lis[i]
+        return ans
