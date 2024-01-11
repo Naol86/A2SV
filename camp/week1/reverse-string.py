8c0b0231a90e983b6f7aa40136c4ad0b15bb3a66
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-01-11 09:13:26 +0300

    Added solution for Reverse String

--- camp/week1/reverse-string.py
+++ camp/week1/reverse-string.py
@@ -3,7 +3,5 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        lis = s.copy()
-        leg = len(lis) - 1
-        for i in range(len(lis)):
-            s[i] = lis[leg - i]
+        for i in range(len(s)//2):
+            s[i],s[-i-1] = s[-i-1],s[i]
