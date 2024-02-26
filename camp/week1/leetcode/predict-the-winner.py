6f76e44393a014b874deba0a61129ddcbdc4dd9d
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-02-26 16:32:31 +0300

    Add solution for Predict the Winner

---
+++ camp/week1/leetcode/predict-the-winner.py
@@ -0,0 +1,17 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        def back_tracking(array, turn):
+            if len(array) == 0:
+                return 0
+            if turn:
+                a = array[0] - back_tracking(array[1:], not turn)
+                b = array[-1] - back_tracking(array[:-1], not turn)
+                return max(a, b)
+            else:
+                a = array[0] - back_tracking(array[1:], not turn)
+                b = array[-1] - back_tracking(array[:-1], not turn)
+                return max(a, b)
+        ans = (back_tracking(nums, True))
+        if ans < 0:
+            return False
+        return True
