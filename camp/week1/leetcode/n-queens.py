0b0e75de1e7bb8e5381ffc0a056e2e9ac2efc75b
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-03-01 16:07:41 +0300

    Add solution for N-Queens

---
+++ camp/week1/leetcode/n-queens.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        col = []
+        nag_digo = [] # r - c
+        pos_digo = [] # r + c
+        board = [["." ]* n for i in range(n)]
+
+        def back_tracking(r):
+            if r == n:
+                temp = ["".join(row) for row in board]
+                ans.append(temp)
+                return
+
+            for c in range(n):
+                if c in col or (c + r) in pos_digo or (c - r) in nag_digo:
+                    continue
+
+                col.append(c)
+                pos_digo.append(c + r)
+                nag_digo.append(c - r)
+                board[r][c] = "Q"
+
+                back_tracking(r + 1)
+
+                col.remove(c)
+                pos_digo.remove(c + r)
+                nag_digo.remove(c - r)
+                board[r][c] = "."
+
+        back_tracking(0)
+        return ans
