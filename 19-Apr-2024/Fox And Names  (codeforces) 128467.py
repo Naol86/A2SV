934302ffee78f39076dce48abdcc8e474933f00f
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-04-19 22:07:40 +0300

    Created 'Fox And Names  (codeforces)' by A2SV Hub

---
+++ 19-Apr-2024/Fox And Names  (codeforces) 128467.py
@@ -0,0 +1,48 @@
+# Problem: Fox And Names  (codeforces) - https://codeforces.com/contest/510/problem/C
+
+from collections import defaultdict, deque
+
+
+graph = defaultdict(list)
+inDegree = [0] * 26
+lis = []
+ans = []
+for _ in range(int(input())):
+  lis.append(input())
+def get_dependency(n):
+  _min = min(len(lis[n]), len(lis[n+1]))
+  for i in range(_min):
+    if lis[n][i] != lis[n+1][i]:
+      graph[lis[n][i]].append(lis[n+1][i])
+      inDegree[ord(lis[n+1][i]) - ord('a')] += 1
+      break
+  else:
+    if len(lis[n]) > len(lis[n+1]):
+      print("Impossible")
+      exit()
+
+for i in range(len(lis) - 1):
+  get_dependency(i)
+
+
+visited = [0] * 26
+for i in range(26):
+  if inDegree[i] != 0 or visited[i] != 0:
+    continue
+  stack = deque([chr(ord('a') + i)])
+  # ans.append(chr(ord('a') + i))
+  while stack:
+    x = stack.popleft()
+    visited[ord(x) - ord('a')] += 1
+    ans.append(x)
+    for node in graph[x]:
+      inDegree[ord(node) - ord('a')] -= 1
+      if inDegree[ord(node) - ord('a')] == 0:
+        stack.append(node)
+        # ans.append(node)
+if len(ans) != 26:
+  print('Impossible')
+else:
+  print(''.join(ans))
+# print(inDegree)
+# print(ans, len(ans))
