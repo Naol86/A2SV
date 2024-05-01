4f279245d7cf295c68727388cdb0b2f1ac037769
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-05-01 21:24:02 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 01-May-2024/Fedor and New Game 138659.py
@@ -0,0 +1,22 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int, input().split())
+
+nums = []
+for _ in range(m + 1):
+  nums.append(int(input()))
+
+count = 0
+i = 0
+while i < m:
+  temp = nums[-1] ^ nums[i]
+  length = 0
+  while temp > 0:
+    if temp & 1 == 1:
+      length += 1
+    temp >>= 1
+  # temp = temp.bit_count()
+  if length <= k:
+    count += 1
+  i += 1
+print(count)
