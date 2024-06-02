26eaa05f21db9de67d9e78a74fc176f22400c893
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-02 07:06:36 +0300

    Created 'Find Pivot Index' by A2SV Hub

---
+++ 02-Jun-2024/Find Pivot Index 163219.py
@@ -0,0 +1,13 @@
+# Problem: Find Pivot Index - https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        lis1 = nums.copy()
+        lis2 = nums.copy()
+        for i in range(1,len(nums)):
+            lis1[i] = lis1[i-1] + lis1[i]
+            lis2[-i-1] = lis2[-i-1] + lis2[-i]
+        for i in range(len(nums)):
+            if lis1[i] == lis2[i]:
+                return i
+        return -1
