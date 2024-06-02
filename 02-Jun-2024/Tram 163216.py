c08e011b41f8137852e5b1f928e12d021ed8b3a9
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-02 06:59:51 +0300

    Created 'Tram' by A2SV Hub

---
+++ 02-Jun-2024/Tram 163216.py
@@ -0,0 +1,9 @@
+# Problem: Tram - https://codeforces.com/problemset/problem/116/A
+
+_max = 0
+_sum = 0
+for _ in range(int(input())):
+  a, b = map(int, input().split())
+  _sum += b - a
+  _max = max(_max, _sum)
+print(_max)
