8a1cf5be63043548184daf20bfc8832bbb0f938c
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-14 22:47:05 +0300

    Created ' King Escape' by A2SV Hub

---
+++ 14-Jun-2024/ King Escape 168462.py
@@ -0,0 +1,15 @@
+# Problem:  King Escape - https://codeforces.com/problemset/problem/1033/A
+
+x = int(input())
+ax, ay = map(int, input().split())
+bx, by = map(int, input().split())
+cx, cy = map(int, input().split())
+
+bx -= ax
+cx -= ax
+by -= ay
+cy -= ay
+if ((bx < 0 )== (cx < 0)) and ((by < 0) == (cy < 0)):
+  print("YES")
+else:
+  print("NO")
