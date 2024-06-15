bcdee94064cf6151ad93c6df5d097d170977b869
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-15 22:13:27 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 15-Jun-2024/New Year Transportation 168692.py
@@ -0,0 +1,16 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+# using map
+# m, t = map(int, input().split())
+m, t = [int(i) for i in input().split()]
+
+nums = [int(i) for i in input().split()]
+
+i = 1
+while i < t:
+  i += nums[i-1]
+  if i == t:
+    print("YES")
+    break
+else:
+  print("NO")
