95a983c3455b25e3ff252936fa0b443bf92899d1
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-18 22:16:00 +0300

    Created 'Can I Square?' by A2SV Hub

---
+++ 18-Jun-2024/Can I Square? 169730.py
@@ -0,0 +1,15 @@
+# Problem: Can I Square? - https://codeforces.com/contest/1915/problem/C
+
+import math
+
+
+for _ in range(int(input())):
+    x = int(input())
+    lis = [int(i) for i in input().split()]
+    
+    _sum = sum(lis)
+    x = int(math.sqrt(_sum))
+    if math.pow(x, 2) == _sum:
+        print("YES")
+    else:
+        print("NO")
