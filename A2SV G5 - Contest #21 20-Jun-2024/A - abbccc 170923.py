cd96d0b1597252f5190d45d949b1fe065a1cff7e
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-20 23:44:40 +0300

    Created 'A - abbccc' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/A - abbccc 170923.py
@@ -0,0 +1,15 @@
+# Problem: A - abbccc - https://codeforces.com/gym/530187/problem/A
+
+x = int(input())
+s = input()
+
+lis = []
+
+i = 1
+j = 0
+while j < x:
+  lis.append(s[j])
+  j += i
+  i += 1
+
+print(''.join(lis))
