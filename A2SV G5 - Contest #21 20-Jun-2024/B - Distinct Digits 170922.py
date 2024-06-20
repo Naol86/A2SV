1e9d67b580063f6ac98a4adf982d436339b54e18
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-06-20 23:44:27 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 20-Jun-2024/B - Distinct Digits 170922.py
@@ -0,0 +1,17 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+for _ in range(int(input())):
+  x = int(input())
+  lis = []
+
+  _max = 9
+
+  while _max < x:
+    lis.append(str(_max))
+    x -= _max
+    _max -= 1
+
+  lis.append(str(x))
+  num = int("".join(lis[::-1]))
+
+  print(num)
