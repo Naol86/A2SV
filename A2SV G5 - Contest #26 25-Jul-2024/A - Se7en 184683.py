08db6d1fe37daa54436bb61da3648e1344d642d2
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-07-25 21:16:11 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/A - Se7en 184683.py
@@ -0,0 +1,14 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+for _ in range(int(input())):
+  x = int(input())
+  if x % 7 == 0:
+    print(x)
+  else:
+    rem = x % 7
+    ten = x - (x%10)
+    ten += 10
+    if x - rem + 7 < ten:
+      print(x - rem + 7)
+    else:
+      print(x - rem)
