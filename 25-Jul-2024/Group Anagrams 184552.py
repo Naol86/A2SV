4894e28546afa91d6813e3c440839eb86fc71983
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-07-25 18:08:07 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 25-Jul-2024/Group Anagrams 184552.py
@@ -0,0 +1,11 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = defaultdict(list)
+        for i in strs:
+            count = [0] * 26
+            for j in i:
+                count[ord(j) - ord('a')] += 1
+            dic[tuple(count)].append(i)
+        return dic.values()
