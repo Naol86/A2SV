99b3ad0550102d5b7b9ef05fb52f9989570679d2
Lij Naol <135800478+Naol86@users.noreply.github.com>
2025-02-03 11:50:22 -0500

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 03-Feb-2025/Way Too Long Words 236952.py
@@ -0,0 +1,8 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+for _ in range(int(input())):
+  s = input()
+  if len(s) < 11:
+    print(s)
+  else:
+    print(f"{s[0]}{len(s) - 2}{s[-1]}")
