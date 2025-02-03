e5ab43bc133580bc95694c33926d11a7e3e8cc05
Lij Naol <135800478+Naol86@users.noreply.github.com>
2025-02-03 11:51:02 -0500

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 236954.py
@@ -0,0 +1,8 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+ans = 0
+for _ in range(int(input())):
+  nums = [int(i) for i in input().split()]
+  if sum(nums) > 1:
+    ans +=1 
+print(ans)
